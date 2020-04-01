8f2f6cf4bcb333306f9926f6f638cc8a2595b041
JR <zorael@gmail.com>
2020-04-01 13:56:22 +0200

    Move semver.d and uda.d back into the source tree

---
+++ source/lu/semver.d
@@ -0,0 +1,20 @@
+/++
+ +  SemVer information about the current release.
+ +
+ +  Contains only definitions, no code. Helps importing projects tell what
+ +  features are available.
+ +/
+module lu.semver;
+
+
+/// SemVer versioning of this build.
+enum LuSemVer
+{
+    majorVersion = 0,  /// SemVer major version of the library.
+    minorVersion = 2,  /// SemVer minor version of the library.
+    patchVersion = 3,  /// SemVer patch version of the library.
+}
+
+
+/// Pre-release SemVer subversion of this build.
+enum luSemVerPrerelease = string.init;
---
+++ source/lu/uda.d
@@ -0,0 +1,31 @@
+/++
+ +  User-defined attributes (UDAs) used here and there.
+ +/
+module lu.uda;
+
+/// UDA conveying that a field may not be serialised to configuration files.
+struct Unconfigurable;
+
+/// UDA conveying that a string is an array with this token as separator.
+struct Separator
+{
+    /// Separator, can be more than one character.
+    string token = ",";
+}
+
+/++
+ +  UDA conveying that this member contains sensitive information and should not
+ +  be printed in clear text; e.g. passwords.
+ +/
+struct Hidden;
+
+/++
+ +  UDA conveying that a string contains characters that could otherwise
+ +  indicate a comment.
+ +/
+struct CannotContainComments;
+
+/++
+ +  UDA conveying that this member's value must be quoted when serialised.
+ +/
+struct Quoted;
