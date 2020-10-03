19b8d83641e076af0a712c6e92e2c3d11da6cd64
JR <zorael@gmail.com>
2020-10-03 16:46:25 +0200

    v0.5.0
    This release removes the lu.net module. It was deemed too specific to
    importing projects to warrant being in general purpose lu. Additionally
    it pulled the requests dependency, which only made sense if you wanted
    to use lu.net (and was baggage otherwise).
    
    As this is obviously a breaking change, this bumps the minor version.
    
    No other significant changes.

--- source/lu/semver.d
+++ source/lu/semver.d
@@ -11,8 +11,8 @@ module lu.semver;
 enum LuSemVer
 {
     majorVersion = 0,  /// SemVer major version of the library.
-    minorVersion = 4,  /// SemVer minor version of the library.
-    patchVersion = 1,  /// SemVer patch version of the library.
+    minorVersion = 5,  /// SemVer minor version of the library.
+    patchVersion = 0,  /// SemVer patch version of the library.
 }
 
 
