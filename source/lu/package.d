5cd84bba78e4e7df7b545ba6ac4f1bc85bbe1efd
JR <zorael@gmail.com>
2025-01-26 15:06:47 +0100

    cosmetics: straggling ddoc

--- source/lu/package.d
+++ source/lu/package.d
@@ -1,8 +1,6 @@
 /++
     $(B lu) is a general purpose library for the D programming language.
 
-    It complements the standard library and is not a replacement for it.
-
     Copyright: [JR](https://github.com/zorael)
     License: [Boost Software License 1.0](https://www.boost.org/users/license.html)
 
