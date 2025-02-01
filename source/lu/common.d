ef5a04cd22bbcc738a1f42b988d67ddf52d72c0c
JR <zorael@gmail.com>
2025-02-01 10:56:24 +0100

    Fix lu.common ddoc not being generated by adrdox

--- source/lu/common.d
+++ source/lu/common.d
@@ -1,13 +1,15 @@
+// This looks weird but adrdox doesn't pick the ddoc up otherwise
+
+deprecated("Module was renamed; import symbols from `lu.misc` instead")
 /++
     Compatibility module publicly importing symbols from [lu.misc].
     Import symbols directly from [lu.misc] instead.
 
     This module will be removed in a future release.
  +/
-deprecated("Module was renamed; import symbols from `lu.misc` instead")
 module lu.common;
 
-/+
+/++
     Publicly import all of `lu.misc`.
  +/
 public import lu.misc;
